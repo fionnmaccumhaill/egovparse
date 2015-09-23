912467ab899ed98c0e79d5138f17f078e7d34074
fionnmaccumhaill <edward.foley@comcast.net>
2015-09-22 20:54:45 -0400

    Reogranzing project

--- egovparse.rb
+++ egovparse.rb
@@ -1,6 +1,28 @@
 require 'rubygems'
 require 'fileutils'
-require 'lib/ctdata_create_index.rb'
+
+puts "starting" 
+
+require './lib/CTDataIndexModule'
+require './lib/CTDataRetrieveModule'
+
+JSON_FILE_CONST = "dataidx.json"
+JSON_DIR_CONST = "datafiles/"
+
+def createIndex()
+    tm = Object.new
+    tm.extend(CTDataIndexModule)
+    tm.echo("creating index")
+    tm.createIdx()
+end
+
+def retrieveData()
+    tm = Object.new
+    tm.extend(CTDataRetrieveModule)
+    tm.echo("retriveing data")
+    tm.echo("something something something else")
+    tm.retrieve(JSON_DIR_CONST+JSON_FILE_CONST, JSON_DIR_CONST)
+end
 
 # Ruby application code.
 # This code is not ready to run and is just a placeholder at present.
@@ -11,3 +33,12 @@ pathName=ARGV[1]
 puts "App Name:"+appName.to_s+" path:"+pathName.to_s
 puts "last char:"+pathName[-1, 1]
 
+if ARGV[0] == "index"
+    createIndex()
+end
+
+if ARGV[0] == "retrieve"
+    retrieveData()
+end
+
+puts "done"
